848b00b1f87924a4c2c9eea9ec9f622516586252
j2cey <jud10parfait@gmail.com>
2021-08-03 11:39:36 +0000

    Test ARIS Stored Proc 3

--- app/Models/Aris.php
+++ app/Models/Aris.php
@@ -12,6 +12,7 @@ class Aris extends Model
     use HasFactory;
 
     public static function getChequeInfos($reference) {
-        return DB::connection("sqlsrv")->statement('exec _Encaissements_soumis_pour_chequesimpayes @reference='.$reference);
+        //return DB::connection("sqlsrv")->statement('exec _Encaissements_soumis_pour_chequesimpayes @reference='.$reference);
+        return DB::select('EXEC dbo._Encaissements_soumis_pour_chequesimpayes ?', array($reference));
     }
 }
